ec3a450b08977ed553bea0f627a8efc56f8a4a42
brenda njoki <ngunjiribrenda5@gmail.com>
2025-06-29 20:40:35 +0300

    updates the add function

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ function displayPosts() {
       postList.innerHTML = "";
       //const sidebar = document.getElementById("sidebar-posts");
       posts.forEach((post) => {
+        postList.innerHTML += `
+        <div data-id ="${post.id}">
+        <h3>${post.title}</h3>
+        <p>${post.content}</p>
+        <small>By: ${post.author}</small
+        <button onclick="deletePost(${post.id})">Delete</button>
+        </div>
+        `;
+
         const postDiv = document.createElement("div");
         postDiv.textContent = post.title;
         postDiv.addEventListener("click", () => handlePostClick(post.id));
@@ -30,5 +39,55 @@ function handlePostClick(postId) {
       document.getElementById("post-author").textContent = `By: ${post.author}`;
     });
 }
+function addNewPostListener() {
+  const form = document.getElementById("new-post-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-document.addEventListener("DOMContentLoaded", main);
+    const title = form.title.value;
+    const content = form.content.value;
+    const author = form.author.value;
+
+    const newPost = {
+      title,
+      content,
+      author,
+    };
+    fetch("http://localhost:3000/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPost),
+    })
+      .then(res => res.json())
+      .then(() => {
+        form.reset();
+        displayPosts();
+      })
+      .catch((error) => console.error("Error adding post:", error));
+  });
+}
+document.addEventListener("DOMContentLoaded",() => {
+  displayPosts();
+  addNewPostListener();
+});
+
+// function deletePost(id) {
+//   fetch(`http://localhost:3000/posts/${id}`, {
+//     method: "DELETE",
+//   })
+//     .then(res => {
+//       if (res.ok) {
+//         displayPosts();
+//       } else {
+//         alert("Failed to delete post. ");
+//       }
+//     })
+//     .catch((errr) => {
+//       console.error("Error:", err);
+//     });
+// }
+// document.addEventListener("DOMContentLoaded", () => {
+//   displayPosts();
+// });
